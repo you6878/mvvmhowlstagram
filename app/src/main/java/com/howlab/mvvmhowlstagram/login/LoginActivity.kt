38626b7b54fb309fb5c5a9171b12b3b215765297
Howl <you6878@icloud.com>
2023-02-08 16:24:06 +0900

    EP04 - Mvvm 안드로이드 인스타그램 앱 만들기 - Google Login

--- app/src/main/java/com/howlab/mvvmhowlstagram/login/LoginActivity.kt
+++ app/src/main/java/com/howlab/mvvmhowlstagram/login/LoginActivity.kt
@@ -3,15 +3,18 @@ package com.howlab.mvvmhowlstagram.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.howlab.mvvmhowlstagram.R
 import com.howlab.mvvmhowlstagram.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    lateinit var auth : FirebaseAuth
+
     lateinit var binding : ActivityLoginBinding
     val loginViewModel: LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +23,6 @@ class LoginActivity : AppCompatActivity() {
         binding.viewModel = loginViewModel
         binding.activity = this
         binding.lifecycleOwner = this
-        auth = FirebaseAuth.getInstance()
         setObserve()
 
     }
@@ -37,20 +39,21 @@ class LoginActivity : AppCompatActivity() {
             }
         }
     }
-    fun loginWithSignupEmail(){
-        println("Email")
-        auth.createUserWithEmailAndPassword(loginViewModel.id.value.toString(),loginViewModel.password.value.toString()).addOnCompleteListener {
-            if(it.isSuccessful){
-                loginViewModel.showInputNumberActivity.value = true
-            }else{
-                //아이디가 있을경우
 
-            }
-        }
-
-    }
     fun findId(){
         println("findId")
         loginViewModel.showFindIdActivity.value = true
     }
+
+    //구글로그인이 성공한 결과값 받는 함수
+    var googleLoginResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+
+        val data = result.data
+        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+        val account = task.getResult(ApiException::class.java)
+         // 로그인한 사용자 정보를 암호화한 값
+        loginViewModel.firebaseAuthWithGoogle(account.idToken)
+    }
+
 }
--- app/src/main/java/com/howlab/mvvmhowlstagram/login/LoginViewModel.kt
+++ app/src/main/java/com/howlab/mvvmhowlstagram/login/LoginViewModel.kt
@@ -1,12 +1,62 @@
 package com.howlab.mvvmhowlstagram.login
 
+import android.app.Application
+import android.view.View
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.howlab.mvvmhowlstagram.R
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
+    var auth  = FirebaseAuth.getInstance()
     var id : MutableLiveData<String> = MutableLiveData("howl")
     var password : MutableLiveData<String> = MutableLiveData("")
 
     var showInputNumberActivity : MutableLiveData<Boolean> = MutableLiveData(false)
     var showFindIdActivity : MutableLiveData<Boolean> = MutableLiveData(false)
+    val context = getApplication<Application>().applicationContext
+
+    var googleSignInClient : GoogleSignInClient
+    init {
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(context.getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(context,gso)
+
+    }
+
+    fun loginWithSignupEmail(){
+        println("Email")
+        auth.createUserWithEmailAndPassword(id.value.toString(),password.value.toString()).addOnCompleteListener {
+            if(it.isSuccessful){
+                showInputNumberActivity.value = true
+            }else{
+                //아이디가 있을경우
+
+            }
+        }
+
+    }
+    fun loginGoogle(view : View){
+        var i = googleSignInClient.signInIntent
+        (view.context as? LoginActivity)?.googleLoginResult?.launch(i)
+    }
+    fun firebaseAuthWithGoogle(idToken : String?){
+        val credential = GoogleAuthProvider.getCredential(idToken,null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if(it.isSuccessful){
+                showInputNumberActivity.value = true
+            }else{
+                //아이디가 있을경우
+
+            }
+        }
+    }
 }
